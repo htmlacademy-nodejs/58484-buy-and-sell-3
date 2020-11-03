f647ab5756b9648efc9cabd56fa2e7c69f5faaea
Vasya Ponomarenko <777Vasya77@gmail.com>
2020-11-03 11:11:22 +0200

    Выполнил задание #15. Учебный проект: шаблонизируй это

--- src/express/index.js
+++ src/express/index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const path = require(`path`);
 const express = require(`express`);
 const mainRouter = require(`./routes/main`);
 const myRouter = require(`./routes/my`);
@@ -9,11 +10,28 @@ const DEFAULT_PORT = 8080;
 
 const app = express();
 
+app.set(`views`, path.join(__dirname, `templates`));
+app.set(`view engine`, `pug`);
+
+app.use(express.static(path.join(__dirname, `/public`)));
 app.use(express.json());
 
 app.use(`/`, mainRouter);
 app.use(`/my`, myRouter);
 app.use(`/offers`, offersRouter);
+
+// Error Routes
+app.get(`/500`, (req, res) => {
+  res.render(`errors/500`, {
+    errorType: `server`
+  });
+});
+
+app.get(`*`, (req, res) => {
+  res.render(`errors/400`, {
+    errorType: `not-found`
+  });
+});
 
 app.listen(DEFAULT_PORT, () => {
   console.log(`Сервер запущен на порту: ${DEFAULT_PORT}`);
